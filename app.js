057742292d325d0e7673caae40eeb03474369687
vikaskaroia <vkaskarotia@gmail.com>
2023-06-05 01:18:20 +0530

    credentials

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookieParser())
 app.use(cors({
     origin:[process.env.FRONTEND_URL],
     methods:["GET","PUT","POST","DELETE"],
-    credential:true,
+    credentials:true,
 }))
 
 //using routes

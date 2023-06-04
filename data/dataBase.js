c946bd588f090d57053f25ed9b3933fd3a2ae9ae
vikaskaroia <vkaskarotia@gmail.com>
2023-06-04 15:24:40 +0530

    engine fix

--- data/dataBase.js
+++ data/dataBase.js
@@ -1,8 +1,8 @@
  import mongoose from "mongoose";
 
 export const connectDb=()=>{
-    mongoose.connect(process.env.MONGO_URI ,
-{dbName:"todoApp"})
+    mongoose.connect(process.env.MONGO_URI ,{dbName:"todoApp"}
+)
 .then(()=>{console.log("database is connected")})
 .catch((e)=>{console.log(e)})
 
